add0f3dfad4ed258fe5e962dca9398edeab95d31
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-02-21 18:33:59 +0530

    command line code

---
+++ _CODE_2/command_line.java
@@ -0,0 +1,21 @@
+class command_line
+{
+      public static void main(String []args)
+      {
+            
+ 
+            int a=Integer.parseInt(args[0]);
+            int b=Integer.parseInt(args[1]);
+ 
+            int c=a+b;
+            System.out.println("sum of " + a + " and " + b +" is " + c);
+      }
+}
+
+
+
+
+
+
+--->as here we have passed the string type arguement in stati void main method
+we have to convert that into int type variable  so we can add the two numbers that we have to add.
