c31a8ecaa956a09af16a083bbd11cc913541a31b
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-02-27 22:58:14 +0530

    overload_main_method()

---
+++ _CODE_8/overloading_main_method.java
@@ -0,0 +1,18 @@
+public class A
+{
+   public static void main(String[]args)
+   {
+     A object=new A();
+     object.main();
+     
+   }
+  public static void main()
+  {
+  
+     System.out.println("in  2nd main method");
+  
+  }
+
+}
+------------if we want to execute the overloaded main() method it must be called from the original main() method.
+
